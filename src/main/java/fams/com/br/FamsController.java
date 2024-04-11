4530bba8bc1b89635793d680518ea80b897d8c20
ricardogelli <ricardo.gelli@hotmail.com>
2024-04-11 17:05:49 -0300

    Criação da aplicação

--- src/main/java/fams/com/br/FamsController.java
+++ src/main/java/fams/com/br/FamsController.java
@@ -6,10 +6,7 @@ import fams.com.br.repository.DadosFamsRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/fams")
@@ -22,5 +19,10 @@ public class FamsController {
     @Transactional
     public void cadastrarFams(@RequestBody @Valid DadosFamsCadastro famsCadastro){
         repository.save(new DadosFams(famsCadastro));
+    }
+
+    @DeleteMapping
+    public void delete(DadosFams id){
+        repository.delete(id);
     }
 }
